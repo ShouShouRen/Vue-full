786af25924e89e015b7a4ea88c4b928bcc3eae43
Peter Chen <peterchen2017125@gmail.com>
2023-08-05 17:19:41 +0800

    20230805

---
+++ Vue2/Day3/website/src/components/XtxBanner.vue
@@ -0,0 +1,197 @@
+<template>
+  <div class="banner">
+    <div class="wrapper">
+      <!-- 图 -->
+      <ul class="pic">
+        <li>
+          <a href="#"><img src="@/assets/images/banner1.png" alt="" /></a>
+        </li>
+        <li>
+          <a href="#"><img src="@/assets/images/banner1.png" alt="" /></a>
+        </li>
+      </ul>
+      <!-- 侧导航 -->
+      <div class="subnav">
+        <ul>
+          <li>
+            <div>
+              <span><a href="#">生鲜</a></span>
+              <span><a href="#">水果</a><a href="#">蔬菜</a></span>
+            </div>
+            <i class="iconfont icon-arrow-right-bold"></i>
+          </li>
+          <li>
+            <div>
+              <span><a href="#">美食</a></span>
+              <span><a href="#">面点</a><a href="#">干果</a></span>
+            </div>
+            <i class="iconfont icon-arrow-right-bold"></i>
+          </li>
+          <li>
+            <div>
+              <span><a href="#">餐厨</a></span>
+              <span><a href="#">数码产品</a></span>
+            </div>
+            <i class="iconfont icon-arrow-right-bold"></i>
+          </li>
+          <li>
+            <div>
+              <span><a href="#">电器</a></span>
+              <span><a href="#">床品</a><a href="#">四件套</a><a href="#">被枕</a></span>
+            </div>
+            <i class="iconfont icon-arrow-right-bold"></i>
+          </li>
+          <li>
+            <div>
+              <span><a href="#">居家</a></span>
+              <span><a href="#">奶粉</a><a href="#">玩具</a><a href="#">辅食</a></span>
+            </div>
+            <i class="iconfont icon-arrow-right-bold"></i>
+          </li>
+          <li>
+            <div>
+              <span><a href="#">洗护</a></span>
+              <span><a href="#">洗发</a><a href="#">洗护</a><a href="#">美妆</a></span>
+            </div>
+            <i class="iconfont icon-arrow-right-bold"></i>
+          </li>
+          <li>
+            <div>
+              <span><a href="#">孕婴</a></span>
+              <span><a href="#">奶粉</a><a href="#">玩具</a></span>
+            </div>
+            <i class="iconfont icon-arrow-right-bold"></i>
+          </li>
+          <li>
+            <div>
+              <span><a href="#">服饰</a></span>
+              <span><a href="#">女装</a><a href="#">男装</a></span>
+            </div>
+            <i class="iconfont icon-arrow-right-bold"></i>
+          </li>
+          <li>
+            <div>
+              <span><a href="#">杂货</a></span>
+              <span><a href="#">户外</a><a href="#">图书</a></span>
+            </div>
+            <i class="iconfont icon-arrow-right-bold"></i>
+          </li>
+          <li>
+            <div>
+              <span><a href="#">品牌</a></span>
+              <span><a href="#">品牌制造</a></span>
+            </div>
+            <i class="iconfont icon-arrow-right-bold"></i>
+          </li>
+        </ul>
+      </div>
+      <!-- 指示器 -->
+      <ol>
+        <li class="current"><i></i></li>
+        <li><i></i></li>
+        <li><i></i></li>
+      </ol>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.banner {
+  height: 500px;
+  background-color: #F5F5F5;
+
+  .wrapper {
+    position: relative;
+    overflow: hidden;
+  }
+
+  .pic {
+    display: flex;
+    width: 3720px;
+    height: 500px;
+
+    li {
+      width: 1240px;
+      height: 500px;
+    }
+  }
+
+  .subnav {
+    position: absolute;
+    left: 0;
+    top: 0;
+    width: 250px;
+    height: 500px;
+    background-color: rgba(0, 0, 0, 0.42);
+
+    li {
+      display: flex;
+      justify-content: space-between;
+      padding: 0 20px 0 30px;
+      height: 50px;
+      line-height: 50px;
+    }
+
+    a,
+    i {
+      color: #fff;
+    }
+
+    li {
+      span {
+        &:nth-child(1) {
+          margin-right: 14px;
+        }
+
+        &:nth-child(2) a {
+          margin-right: 5px;
+          font-size: 14px;
+        }
+      }
+
+      &:hover {
+        background-color: #00BE9A;
+      }
+    }
+  }
+
+  ol {
+    position: absolute;
+    right: 17px;
+    bottom: 17px;
+    display: flex;
+
+    li {
+      cursor: pointer;
+      margin-left: 8px;
+      padding: 4px;
+      width: 22px;
+      height: 22px;
+      background-color: transparent;
+      border-radius: 50%;
+
+      i {
+        display: block;
+        width: 14px;
+        height: 14px;
+        background-color: rgba(255, 255, 255, 0.5);
+        border-radius: 50%;
+      }
+    }
+
+    .current {
+      background-color: rgba(255, 255, 255, 0.5);
+
+      i {
+        background-color: #fff;
+      }
+    }
+  }
+}
+</style>
---
+++ Vue2/Day3/website/src/components/XtxHeaderNav.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="header wrapper">
+    <!-- logo -->
+    <div class="logo">
+      <h1>
+        <a href="#">小兔鲜儿</a>
+      </h1>
+    </div>
+    <!-- 导航 -->
+    <div class="nav">
+      <ul>
+        <li><a href="#">首页</a></li>
+        <li><a href="#">生鲜</a></li>
+        <li><a href="#">美食</a></li>
+        <li><a href="#">餐厨</a></li>
+        <li><a href="#">电器</a></li>
+        <li><a href="#">居家</a></li>
+        <li><a href="#">洗护</a></li>
+        <li><a href="#">孕婴</a></li>
+        <li><a href="#">服装</a></li>
+      </ul>
+    </div>
+    <!-- 搜索 -->
+    <div class="search">
+      <span class="iconfont icon-search"></span>
+      <input type="text" placeholder="搜一搜" />
+    </div>
+    <!-- 购物车 -->
+    <div class="cart">
+      <span class="iconfont icon-cart-full"></span>
+      <i>2</i>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  margin: 22px auto;
+
+  .logo {
+    margin-right: 40px;
+    width: 200px;
+    height: 88px;
+    background-color: pink;
+
+    a {
+      display: block;
+      width: 200px;
+      height: 88px;
+      background-image: url(~@/assets/images/logo.png);
+      font-size: 0;
+    }
+  }
+
+  .nav {
+    margin-top: 33px;
+    margin-right: 27px;
+
+    ul {
+      display: flex;
+    }
+
+    li {
+      margin-right: 48px;
+    }
+
+    a {
+      display: block;
+      height: 34px;
+
+      &:hover {
+        border-bottom: 2px solid #5EB69C;
+      }
+    }
+  }
+
+  .search {
+    display: flex;
+    margin-right: 45px;
+    margin-top: 33px;
+    width: 170px;
+    height: 34px;
+    border-bottom: 2px solid #F4F4F4;
+
+    .icon-search {
+      margin-right: 8px;
+      font-size: 20px;
+      color: #999;
+    }
+
+    input {
+      flex: 1;
+
+      &::placeholder {
+        color: #ccc;
+      }
+    }
+  }
+
+  .cart {
+    position: relative;
+    margin-top: 33px;
+
+    .icon-cart-full {
+      font-size: 24px;
+    }
+
+    i {
+      position: absolute;
+
+      /* right: -5px; */
+      left: 15px;
+      top: 0;
+      padding: 0 5px;
+      height: 15px;
+      background-color: #E26237;
+      border-radius: 7px;
+      font-size: 12px;
+      color: #fffefe;
+      line-height: 15px;
+    }
+  }
+}
+</style>
