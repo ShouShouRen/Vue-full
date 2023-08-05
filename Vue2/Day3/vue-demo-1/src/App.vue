2cffaf09bc91b5dc65308d43427d5173f0554644
Peter Chen <peterchen2017125@gmail.com>
2023-08-05 12:38:48 +0800

    20230805

--- Vue2/Day3/vue-demo-1/src/App.vue
+++ Vue2/Day3/vue-demo-1/src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <div class="box" @click="fn"></div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  created() {
+    console.log('我是created')
+  },
+  methods: {
+    fn() {
+      alert('hello');
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="less">
+.app {
+  width: 400px;
+  height: 400px;
+  background-color: pink;
+
+  .box {
+    width: 200px;
+    height: 200px;
+    background-color: blue;
+  }
 }
 </style>
--- Vue2/Day3/vue-demo-1/src/main.js
+++ Vue2/Day3/vue-demo-1/src/main.js
@@ -1,8 +1,11 @@
+// 文件和新功能:導入App.vue 基於app.vue創建結構渲染index.html
 import Vue from 'vue'
 import App from './App.vue'
 
+// 當前模式
 Vue.config.productionTip = false
 
 new Vue({
+  // el: '#app',
   render: h => h(App),
 }).$mount('#app')
