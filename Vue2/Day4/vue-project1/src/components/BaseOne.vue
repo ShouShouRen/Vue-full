a73df3975a0f52bdb9dc6b99d09b6fbc981d4d0b
Peter Chen <peterchen2017125@gmail.com>
2023-08-06 11:17:28 +0800

    20230806

---
+++ Vue2/Day4/vue-project1/src/components/BaseOne.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    BaseOne
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+div{
+  border: 3px solid #000;
+  margin: 20px;
+}
+</style>
---
+++ Vue2/Day4/vue-project1/src/components/BaseTwo.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    BaseTwo
+  </div>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style lang="scss" scoped>
+div{
+  border: 1px solid #000;
+  margin: 10px;
+}
+</style>
