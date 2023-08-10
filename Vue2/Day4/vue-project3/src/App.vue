bd8740556904b0e9c54495a6cd3949f36ec3460d
Peter Chen <peterchen2017125@gmail.com>
2023-08-10 09:59:40 +0800

    20230810

---
+++ Vue2/Day4/vue-project3/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app">
+    我是App組件
+    <SonComponent :title="myTitle" @changeTitle="handleChange"></SonComponent>
+  </div>
+</template>
+
+<script>
+import SonComponent from './components/SonComponent.vue';
+export default {
+  data(){
+    return{
+      myTitle: 'some data'
+    }
+  },
+  methods:{
+    handleChange(newTitle){
+      this.myTitle = newTitle
+    }
+  }
+  ,components: {
+    SonComponent
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.app {
+  border: 3px solid #000;
+  margin: 20px;
+  padding: 20px;
+}
+</style>
---
+++ Vue2/Day4/vue-project3/src/components/SonComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="sons">
+    我是Son組件
+    {{ title }}
+    <button @click="changeFn">修改title</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    props:['title'],
+    methods:{
+      changeFn(){
+        this.$emit('changeTitle','我是新的子組件數據')
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+.sons{
+  border: 3px solid #000;
+}
+</style>
