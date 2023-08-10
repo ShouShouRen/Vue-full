b8fc044be815155ae1b70891f2a06b09822655e0
Peter Chen <peterchen2017125@gmail.com>
2023-08-10 12:00:52 +0800

    20230810

---
+++ Vue2/Day4/vue-project4/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <UserInfo :username="username" :age="age" :isSingle="isSingle" :car="car" :hobby="hobby"></UserInfo>
+  </div>
+</template>
+
+<script>
+import UserInfo from './components/UserInfo.vue';
+  export default {
+    data() {
+        return {
+            username: '小帥',
+            age: 18,
+            isSingle: true,
+            car: {
+                brand: '寶馬',
+            },
+            hobby: ['籃球', '羽球', '撞球']
+        };
+    },
+    comments: {
+        UserInfo
+    },
+    components: { UserInfo }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ Vue2/Day4/vue-project4/src/components/UserInfo.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="userinfo">
+    <h3>我是個人信息組件</h3>
+    <div>姓名：{{ username }}</div>
+    <div>年紀：{{ age }}</div>
+    <div>是否單身：{{ isSingle ? '是' : '否' }}</div>
+    <div>車：{{ car.brand }}</div>
+    <div>興趣愛好：{{ hobby.join(',') }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props:['username','age','isSingle','car','hobby']
+  }
+</script>
+
+<style lang="scss" scoped>
+.userinfo{
+  width: 300px;
+  border: 3px solid #000;
+  padding: 20px;
+  div{
+    padding: 10px;
+  }
+}
+</style>
