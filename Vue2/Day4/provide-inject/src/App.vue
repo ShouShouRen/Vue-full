493829d6a1a2596bd4884a1651cfd32a79292e18
Peter Chen <peterchen2017125@gmail.com>
2023-08-15 11:23:47 +0800

    20230815

---
+++ Vue2/Day4/provide-inject/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <button @click="change">修改數據</button>
+    <SonA></SonA>
+    <SonB></SonB>
+  </div>
+</template>
+
+<script>
+import SonA from './components/SonA.vue'
+import SonB from './components/SonB.vue'
+export default {
+  provide() {
+    return {
+      color: this.color,
+      userInfo: this.userInfo,
+    }
+  },
+  data() {
+    return {
+      color: 'pink',
+      userInfo: {
+        name: 'peter',
+        age: 18
+      }
+    }
+  },
+  methods: {
+    change(){
+      // this.color = 'green'
+      this.userInfo.name = 'Chen'
+    }
+  },
+  components: {
+    SonA, SonB
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
