d63c86ac4f2939c76e3d303f41821c65d4e14b1f
Peter Chen <peterchen2017125@gmail.com>
2023-08-15 09:25:42 +0800

    20230815

---
+++ Vue2/Day4/custom-events/src/components/BaseB.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    我是B組件發送方
+    <button @click="clickSend">發送通知</button>
+  </div>
+</template>
+
+<script>
+import Bus from '@/utils/EventBus';
+  export default {
+    methods:{
+      clickSend(){
+        Bus.$emit('sendMsg','事件被觸發了')
+      }
+    },
+  }
+</script>
+
+<style lang="scss" scoped>
+div{
+  width: 200px;
+  height: 150px;
+  padding: 10px;
+  margin-top: 10px;
+  border-radius: 5px;
+  font-size: 20px;
+  border: 2px solid #000;
+}
+</style>
---
+++ Vue2/Day4/custom-events/src/components/BaseC.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    我是C組件接收方
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+import Bus from '../utils/EventBus'
+export default {
+  created() {
+    Bus.$on('sendMsg', (msg) => {
+      // console.log(msg)
+      this.msg = msg
+    })
+  },
+  data() {
+    return {
+      msg: ''
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+div {
+  width: 200px;
+  height: 150px;
+  padding: 10px;
+  margin-top: 10px;
+  border-radius: 5px;
+  font-size: 20px;
+  border: 2px solid #000;
+}
+</style>
