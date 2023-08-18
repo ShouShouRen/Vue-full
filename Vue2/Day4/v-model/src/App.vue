65785df7ca63b45914e4b8f8a64a4083ec728e2a
Peter Chen <peterchen2017125@gmail.com>
2023-08-18 14:38:06 +0800

    20230818

---
+++ Vue2/Day4/v-model/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input v-model="msg1" type="text"> <br><br>
+    <input :value="msg2" @input="msg2 = $event.target.value" type="text">
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        msg1: '',
+        msg2: ''
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
