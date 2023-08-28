a8d7eab6ad0ddb90fe59e5928529a37e27da390a
Peter Chen <peterchen2017125@gmail.com>
2023-08-28 11:35:07 +0800

    20230828

---
+++ Vue2/Day4/ref-refs-2/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app">
+    <BaseFrom ref="baseForm"></BaseFrom>
+    <button @click="handleGet">獲取數據</button>
+    <button @click="handleReset">重設數據</button>
+  </div>
+</template>
+
+<script>
+import BaseFrom from './components/BaseFrom.vue';
+export default {
+  components: {
+    BaseFrom,
+  },
+  methods: {
+    handleGet() {
+      console.log(this.$refs.baseForm.getValues())
+    },
+    handleReset() {
+      this.$refs.baseForm.resetValues()
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ Vue2/Day4/ref-refs-2/src/components/BaseFrom.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="app">
+    <div>
+      账号: <input v-model="account" type="text">
+    </div>
+    <div>
+      密码: <input v-model="password" type="password">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      account: 'account',
+      password: '123456',
+    }
+  },
+  methods: {
+    getValues() {
+      return {
+        account: this.account,
+        password: this.password
+      }
+    },
+    resetValues(){
+      this.account='',
+      this.password=''
+    }
+  }
+}
+</script>
+
+<style scoped>
+.app {
+  border: 2px solid #ccc;
+  padding: 10px;
+}
+
+.app div {
+  margin: 10px 0;
+}
+
+.app div button {
+  margin-right: 8px;
+}
+</style>
