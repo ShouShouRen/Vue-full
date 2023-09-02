e777f7495ff54acb0b474376f1c8d44a8861a69b
Peter Chen <peterchen2017125@gmail.com>
2023-09-02 12:06:46 +0800

    20230902

---
+++ Vue2/Day5/v-customize-value/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1 v-color='color1'>指令值1</h1>
+    <h1 v-color='color2'>指令值2</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      color1: 'red',
+      color2: 'blue'
+    }
+  },
+  directives: {
+    color: {
+      inserted(el, binding) {
+        // console.log(el)
+        el.style.color = binding.value;
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
