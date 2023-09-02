ae8f7207f9f93fcd4981608a20d56b32b4de62e2
Peter Chen <peterchen2017125@gmail.com>
2023-09-02 12:00:16 +0800

    20230902

---
+++ Vue2/Day5/v-customize/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input v-focus ref="inp" type="text">
+  </div>
+</template>
+
+<script>
+  export default {
+    // mounted(){
+    //   this.$refs.inp.focus()
+    // }
+    directives:{
+      focus:{
+        inserted(el){
+          el.focus()
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
