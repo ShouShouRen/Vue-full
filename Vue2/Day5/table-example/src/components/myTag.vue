a6f39378f1608f2d3b5cc21898e94a4ca79eaa9b
Peter Chen <peterchen2017125@gmail.com>
2023-09-10 08:53:55 +0800

    20230910

---
+++ Vue2/Day5/table-example/src/components/myTag.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="my-tag">
+    <input v-if="isEdit" class="input" type="text" placeholder="输入标签" ref="input" v-focus @blur="isEdit = false" />
+    <div class="text" v-else @dblclick="handleClick">
+      茶具
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isEdit: false
+    }
+  },
+  methods: {
+    handleClick() {
+      this.isEdit = true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.my-tag {
+  cursor: pointer;
+
+  .input {
+    appearance: none;
+    outline: none;
+    border: 1px solid #ccc;
+    width: 100px;
+    height: 40px;
+    box-sizing: border-box;
+    padding: 10px;
+    color: #666;
+
+    &::placeholder {
+      color: #666;
+    }
+  }
+}
+</style>
